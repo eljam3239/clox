5b37c39955f784ace30d63d2c75c45fba09581aa
eljam3239 <elijames2301@gmail.com>
2024-10-14 21:50:03 -0400

    Write value arrays

--- chunk.h
+++ chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,
@@ -10,11 +11,13 @@ typedef enum {
 typedef struct {
     int count;
     int capacity;
-    uint8_t* code;  // Dynamic array of bytes       
+    uint8_t* code;  // Dynamic array of bytes    
+    ValueArray constants;   
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
