1c9b67256b63b3810904fc6eb2895145b54a7f29
eljam3239 <elijames2301@gmail.com>
2024-10-15 22:29:04 -0400

    Write constant intruction

--- debug.c
+++ debug.c
@@ -10,6 +10,13 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+}
+
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
@@ -20,6 +27,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
