9cfa93718cee387d3c677c5ca5644336827a3dda
eljam3239 <elijames2301@gmail.com>
2024-10-15 22:37:41 -0400

    Add line to write chunk

--- chunk.h
+++ chunk.h
@@ -13,12 +13,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;  // Dynamic array of bytes    
+    int* lines;     // Dynamic array of lines
     ValueArray constants;   
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
